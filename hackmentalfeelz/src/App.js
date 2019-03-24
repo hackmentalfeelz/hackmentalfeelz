3fd37a06bd98e9dbc738a91db5daa5e784ff79fe
Marat Nepomnyashy <maratbn@gmail.com>
2019-03-24 11:10:56 -0700

    Added logic to add journal entries.

--- hackmentalfeelz/src/App.js
+++ hackmentalfeelz/src/App.js
@@ -6,6 +6,9 @@ class App extends Component {
     super();
 
     this.state = {
+      counters: {
+        journal_entries: 0
+      },
       affirmations: [
 
       ],
@@ -17,7 +20,10 @@ class App extends Component {
         name: "Tom"
       }, {
         name: "Ben"
-      }]
+      }],
+      journal_entries: [
+
+      ]
     };
   }
   render() {
@@ -34,6 +40,12 @@ class App extends Component {
           }
         </ul>
         <h2>Your journal:</h2>
+        <ul>
+          {
+            this.state.journal_entries.map(
+              (objEntry, index) => <li key={ index }>{ objEntry.text }</li>)
+          }
+        </ul>
         <h2>New entry:</h2>
         <div>
           <textarea rows={ 5 } cols={ 40 } value={ this.state.pending_entry }
@@ -45,7 +57,23 @@ class App extends Component {
             }}
           />
         </div>
-        <button>Add</button>
+        <button onClick={ () => {
+          const strNewEntry = this.state.pending_entry;
+          if (!strNewEntry) return;
+
+          this.setState({
+            ...this.state,
+            counters: {
+              ...this.state.counters,
+              journal_entries: this.state.counters.journal_entries + 1
+            },
+            journal_entries: this.state.journal_entries.concat([{
+              id: this.state.counters.journal_entries,
+              text: strNewEntry
+            }]),
+            pending_entry: ""
+          });
+        }}>Add</button>
       </div>
     );
   }

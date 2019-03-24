8ee4e054b8b781b7a9b2194b3eb0cf60aecfdbcb
Marat Nepomnyashy <maratbn@gmail.com>
2019-03-24 10:55:19 -0700

    Added more app skeleton.

--- hackmentalfeelz/src/App.js
+++ hackmentalfeelz/src/App.js
@@ -3,16 +3,43 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      affirmations: [
+
+      ],
+      affirmers: [{
+        name: "Jane"
+      }, {
+        name: "Marsha"
+      }, {
+        name: "Tom"
+      }, {
+        name: "Ben"
+      }]
+    };
+  }
   render() {
     return (
       <div>
-        <h2>Affirmations:</h2>
+        <h1>Welcome Joe!</h1>
+        <h2>Your affirmations:</h2>
+        <h2>Your affirmers with additional affirmations:</h2>
         <ul>
-          <li>Jane</li>
-          <li>Marsha</li>
-          <li>Tom</li>
-          <li>Ben</li>
+          { this.state.affirmers.map(
+              (objAffirmer, index) => <li key={ index }>
+                {objAffirmer.name}
+              </li>)
+          }
         </ul>
+        <h2>Your journal:</h2>
+        <h2>New entry:</h2>
+        <div>
+          <textarea rows={ 5 } cols={ 40 } />
+        </div>
+        <button>Add</button>
       </div>
     );
   }
